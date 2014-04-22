636cd4f68e2f2f2502a640a3e9279788322412b0
aznashwan <aznashwan@yahoo.com>
2014-04-22 17:30:13 +0300

    PC project

---
+++ DroidPi/src/main/java/com/upt/cti/droidpi/TestsActivity.java
@@ -0,0 +1,83 @@
+package com.upt.cti.droidpi;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+
+
+public class TestsActivity extends ActionBarActivity {
+
+    Button piButton,threadsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tests);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        piButton=(Button) findViewById(R.id.pi_button);
+        threadsButton=(Button) findViewById(R.id.threads_button);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.tests, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_tests, container, false);
+            return rootView;
+        }
+    }
+
+    public void piTest(View view)
+    {
+        Intent intent=new Intent(this,PITestActivity.class);
+        startActivity(intent);
+    }
+
+    public void threadsTest(View view)
+    {
+        Intent intent=new Intent(this,ThreadsTestActivity.class);
+        startActivity(intent);
+    }
+}
