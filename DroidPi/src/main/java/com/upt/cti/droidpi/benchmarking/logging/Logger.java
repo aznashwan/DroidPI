1f50e7baae0623990883cfd18319a7abc66ec60b
aznashwan <aznashwan@yahoo.com>
2014-05-11 21:35:49 +0300

    Added appropriate commenting

--- DroidPi/src/main/java/com/upt/cti/droidpi/benchmarking/logging/Logger.java
+++ DroidPi/src/main/java/com/upt/cti/droidpi/benchmarking/logging/Logger.java
@@ -4,11 +4,16 @@ import com.upt.cti.droidpi.benchmarking.timing.TimeUnit;
 
 public class Logger
 {
+
+    //simply writes the times on stdout; used for debugging purposes
+    @Deprecated
     public static void write(long inputTime)
     {
         System.out.println(inputTime);
     }
 
+    //sequentially outputs a string with all the objects given as input
+    @Deprecated
     public static String write(Object... values)
     {
         String s="";
@@ -19,6 +24,8 @@ public class Logger
         return s;
     }
 
+    //simply outputs an input message together with an input time value
+    @Deprecated
     public static String write(String inS, long inT)
     {
         String s="";
@@ -28,6 +35,7 @@ public class Logger
         return s;
     }
 
+    //returns a string with an input message, an input time and the associated time unit
     public static String write(String inputString, long inputTime, TimeUnit inputTimeUnit)
     {
         String s=" ";
--- DroidPi/src/main/java/com/upt/cti/droidpi/benchmarking/timing/Timer.java
+++ DroidPi/src/main/java/com/upt/cti/droidpi/benchmarking/timing/Timer.java
@@ -11,9 +11,9 @@ public class Timer
         this.timeElapsed=0;
     }
 
+    //sets the internal measured time to the current system time
     public void start()
     {
-        this.timer=0;
         this.timeElapsed=0;
         this.timer=System.nanoTime();
     }
@@ -28,11 +28,15 @@ public class Timer
         return timeElapsed;
     }
 
+    //resumes the timer in case of measuring of segmented sequences of code with a single timer
+    @Deprecated
     public void resume()
     {
         this.timer=System.nanoTime();
     }
 
+    //pauses the timer and returns the time recorded from the last start/resume
+    @Deprecated
     public long pause()
     {
         this.timeElapsed+=System.nanoTime()-timer;
