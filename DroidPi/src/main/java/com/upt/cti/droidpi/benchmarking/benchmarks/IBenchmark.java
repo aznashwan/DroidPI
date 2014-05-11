fda4a72fcfee7d76e7620d78caf4c0f344dc978c
aznashwan <aznashwan@yahoo.com>
2014-05-12 02:38:50 +0300

    Added warmUp() to IBenchmark

--- DroidPi/src/main/java/com/upt/cti/droidpi/benchmarking/benchmarks/IBenchmark.java
+++ DroidPi/src/main/java/com/upt/cti/droidpi/benchmarking/benchmarks/IBenchmark.java
@@ -2,8 +2,18 @@ package com.upt.cti.droidpi.benchmarking.benchmarks;
 
 public interface IBenchmark
 {
+    //will initialize all data required to run the specific test
     public void initialize();
+
+    //will warm up the processor to handle the upandcoming tests
+    public void warmUp();
+
+    //will run the actual test, storing the result as a field in the implementing object
     public void runTest();
+
+    //will return the result of the previously run test
     public long getResult();
+
+    //will return a message with the result of the test neatly expressed
     public String resultMessage();
 }
